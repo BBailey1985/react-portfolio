aa3cd453d6a40619c17e649d2a5957bf560ee589
BBailey1985 <brandon_bailey@hotmail.com>
2022-04-01 11:00:50 -0500

    added Resume pdf and web proficiencies to Resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function Resume() {
-
+  return (
+    <section>
+      <div>
+      <h1 className="h1resume">My Resume</h1>
+      </div>
+      <div className='lower-spacing'>
+      <a href={require("../../assets/files/Web Developer.pdf")} download>
+        <h3 className="h3resume">Download my Resume!</h3>
+      </a>
+      </div>
+      <div>
+      <h3 className="h3resume">Web Development Proficiencies</h3>
+      <ul>
+        <li>Javascript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React.js</li>
+        <li>Node. js</li>
+        <li>MondoDB</li>
+        <li>RESTful API</li>
+        <li>SQL</li>
+        <li>PWA</li>
+      </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Resume;
